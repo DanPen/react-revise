2c42baf8c778708c84b31c50814f2a5c5cc0e142
Daniel Pendergast <danielpendergast@icloud.com>
2017-11-22 20:29:52 -0600

    Begin development of EditHook component

--- modules/EditHook.js
+++ modules/EditHook.js
@@ -1,1 +1,17 @@
-export default 
+import React from 'react'
+import invariant from 'invariant'
+
+class EditHook extends React.Component {
+  state = {
+    editing: false
+  }
+
+  componentWillMount () {
+    const { children } = this.props
+
+    invariant(
+      children == null || React.Children.count(children) === 1,
+      'An <EditHook> may have only one child element'
+    )
+  }
+}
