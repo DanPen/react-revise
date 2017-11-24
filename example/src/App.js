194a58eeedcc2953f65447358459fc9f798419d7
Daniel Pendergast <danielpendergast@icloud.com>
2017-11-24 17:11:33 -0600

    Dump the element.contentEditable approach and instead create <textarea> clones
    HTMLelement.contentEditable is known to be *very* inconsistent. Instead, I made EditorHook to create a <textarea> clone. It currently matches position and dimensions, but will need to make it look identical (fonts, line heights, background-color, etc).

--- example/src/App.js
+++ example/src/App.js
@@ -8,18 +8,22 @@ import Button, { SuperButton } from './Button'
 import { Provider } from 'react-redux'
 
 export default class App extends Component {
+  state = {
+    editable: false
+  }
+
+
+
   render () {
     return (
-      <Provider store={this.props.store}>
-        <div>
-          <EditorHook editable={true}>
-            <Title />
-          </EditorHook>
-          <EditorHook editable={true}>
-            <div>Dolor pariatur reprehenderit minim laborum enim irure qui adipisicing eiusmod. Lorem do eu aute elit cupidatat exercitation et non minim. Laborum nulla dolor voluptate sit officia consectetur fugiat commodo ad cillum est laborum. Enim laborum reprehenderit enim duis ad sunt culpa laboris magna reprehenderit sit laboris et. Amet aliquip duis laborum laborum cillum tempor aliquip ut occaecat aliqua reprehenderit ut. Dolor veniam exercitation occaecat magna dolore aute ipsum ea in in. Non culpa laborum nulla do non consectetur enim do cupidatat nulla proident sint commodo cillum ea. Tempor labore ex excepteur sint ipsum ipsum consectetur cillum cillum occaecat incididunt deserunt excepteur deserunt aliqua pariatur.</div>
-          </EditorHook>
-        </div>
-      </Provider>
+      <div>
+        <input type="checkbox" onChange={(e) => this.setState({
+          editable: e.target.checked
+        })} />
+        <EditorHook editable={this.state.editable}>
+          <div>Dolor pariatur reprehenderit minim laborum enim irure qui adipisicing eiusmod. Lorem do eu aute elit cupidatat exercitation et non minim. Laborum nulla dolor voluptate sit officia consectetur fugiat commodo ad cillum est laborum. Enim laborum reprehenderit enim duis ad sunt culpa laboris magna reprehenderit sit laboris et. Amet aliquip duis laborum laborum cillum tempor aliquip ut occaecat aliqua reprehenderit ut. Dolor veniam exercitation occaecat magna dolore aute ipsum ea in in. Non culpa laborum nulla do non consectetur enim do cupidatat nulla proident sint commodo cillum ea. Tempor labore ex excepteur sint ipsum ipsum consectetur cillum cillum occaecat incididunt deserunt excepteur deserunt aliqua pariatur.</div>
+        </EditorHook>
+      </div>
     )
   }
 }
--- src/EditorHook.js
+++ src/EditorHook.js
@@ -7,13 +7,16 @@ import PropTypes from 'prop-types'
 
 import invariant from 'invariant'
 
+import ReactDOM from 'react-dom'
+
+
 class EditorHook extends Component {
   static propTypes = {
     children: PropTypes.node
   }
 
   componentWillMount () {
-    const { children, editable } = this.props
+    const { children } = this.props
 
     invariant(
       children == null || React.Children.count(children) === 1,
@@ -21,38 +24,67 @@ class EditorHook extends Component {
     )
   }
 
-  renderChildren = () => {
-    const { children } = this.props
+  componentDidMount () {
+    const { children, editable } = this.props
 
-    const newChildren = React.Children.map(children, child => {
+    const childrenArray = React.Children.toArray(children)
+    const realElement = ReactDOM.findDOMNode(this.refs['0'])
+    const imaginaryElement = ReactDOM.findDOMNode(this.refs['1'])
 
-      const additionalProps = this.setRef(child)
+    // Give the imaginary element an absolute position, pointed to where realElement is
+    const boundingBox = realElement.getBoundingClientRect()
+    imaginaryElement.style.cssText +=';'+ `
+      position: absolute;
+      top: ${boundingBox.top}px;
+      left: ${boundingBox.left}px;
+      width: ${boundingBox.width}px;
+      height: ${boundingBox.height}px;
+    `
 
-      return React.cloneElement(child, additionalProps)
-    })
-
-    return newChildren
+    this.toggleEditible(editable, realElement, imaginaryElement)
   }
 
-  setRef = (child) => {
-    var refPropName = 'ref'
+  componentWillReceiveProps (nextProps) {
+    const realElement = ReactDOM.findDOMNode(this.refs['0'])
+    const imaginaryElement = ReactDOM.findDOMNode(this.refs['1'])
 
-    if (child.type.name === 'StyledComponent')
-      refPropName = 'innerRef'
+    this.toggleEditible(nextProps.editable, realElement, imaginaryElement)
+  }
 
-    var additionalProps = {}
-    additionalProps[refPropName] = (ref) => {ref.contentEditable = this.props.editable; this.grabRef(ref)}
+  toggleEditible = (editable) => {
+    const realElement = ReactDOM.findDOMNode(this.refs['0'])
+    const imaginaryElement = ReactDOM.findDOMNode(this.refs['1'])
+    // When in edit mode:
+    // hide real element, show imaginaryElement
+    // Use visibility:hidden on realElement because we don't want the page to re-layout, in the case that it has position: static
+    // Use display:none on imaginaryElement because we have no need to render it at all
 
-    return additionalProps
+    // TODO: The imaginaryElement's value needs to be set everytime we go into edit mode
+    if (editable) {
+      realElement.style.visibility = 'hidden'
+      imaginaryElement.style.display = 'block'
+    } else {
+      realElement.style.visibility = 'visible'
+      imaginaryElement.style.display = 'none'
+    }
+  }
+
+  renderChildren = () => {
+    const { children, editable } = this.props
+
+    // Map a ref to the (currently) single child element and discard the previous child
+    const child = React.Children.only(children)
+    const newChild =  React.cloneElement(child, { ref: 0, key: 0 })
+
+    // Create the editable child
+    const editableChild = <textarea ref='1' key='1' value={newChild.props.children} />
+
+    return [newChild, editableChild]
   }
 
   render() {
-    // We don't want to wrap a div or span around the children, because that could mess up the DOM and visuals
+    // We don't want to wrap a div or span around the children, because that could mess up the DOM due to the disparities of display:block and display:inline
     return this.renderChildren()
-  }
-
-  grabRef = (ref) => {
-    console.log(ref)
   }
 }
 
