4853adf88ae3a9ad948c60d3275f1a97fa899cae
Daniel Pendergast <danielpendergast@icloud.com>
2017-11-24 19:48:18 -0600

    Add an example with paragraphs and spans

--- example/src/App.js
+++ example/src/App.js
@@ -27,6 +27,17 @@ export default class App extends Component {
         <EditorHook editable={this.state.editable}>
           <div>Dolor pariatur reprehenderit minim laborum enim irure qui adipisicing eiusmod. Lorem do eu aute elit cupidatat exercitation et non minim. Laborum nulla dolor voluptate sit officia consectetur fugiat commodo ad cillum est laborum. Enim laborum reprehenderit enim duis ad sunt culpa laboris magna reprehenderit sit laboris et. Amet aliquip duis laborum laborum cillum tempor aliquip ut occaecat aliqua reprehenderit ut. Dolor veniam exercitation occaecat magna dolore aute ipsum ea in in. Non culpa laborum nulla do non consectetur enim do cupidatat nulla proident sint commodo cillum ea. Tempor labore ex excepteur sint ipsum ipsum consectetur cillum cillum occaecat incididunt deserunt excepteur deserunt aliqua pariatur.</div>
         </EditorHook>
+
+        <p>
+          <EditorHook editable={this.state.editable}>
+            <span>
+              Dolor pariatur reprehenderit minim laborum enim irure qui adipisicing eiusmod.
+            </span>
+          </EditorHook>
+          <span>
+            Lorem do eu aute elit cupidatat exercitation et non minim. Laborum nulla dolor voluptate sit officia consectetur fugiat commodo ad cillum est laborum. Enim laborum reprehenderit enim duis ad sunt culpa laboris magna reprehenderit sit laboris et. Amet aliquip duis laborum laborum cillum tempor aliquip ut occaecat aliqua reprehenderit ut. Dolor veniam exercitation occaecat magna dolore aute ipsum ea in in. Non culpa laborum nulla do non consectetur enim do cupidatat nulla proident sint commodo cillum ea. Tempor labore ex excepteur sint ipsum ipsum consectetur cillum cillum occaecat incididunt deserunt excepteur deserunt aliqua pariatur.
+          </span>
+        </p>
       </div>
     )
   }
