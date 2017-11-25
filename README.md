a960b5215f3d6dc846defa6cd6ac912e9041284a
Daniel Pendergast <danielpendergast@icloud.com>
2017-11-24 22:41:40 -0600

    Update example to make it more understandable

--- README.md
+++ README.md
@@ -18,13 +18,34 @@ import React, { Component } from 'react'
 
 import { EditorHook } from 'react-revise'
 
-class Example extends Component {
+class App extends Component {
+  state = {
+    editable: false
+  }
+
   render () {
     return (
-      <EditorHook><h1>Hello, World!</h1></EditorHook>
+      <div>
+        <input id="checkbox" type="checkbox" onChange={(e) => this.setState({
+          editable: e.target.checked
+        })} />
+        <label for="checkbox"> edit</label>
+
+        <EditorHook editable={this.state.editable}>
+          <h1>Hello, World!</h1>
+        </EditorHook>
+
+        <EditorHook editable={this.state.editable}>
+          <p>
+            Tempor mollit dolore culpa occaecat labore voluptate Lorem esse. Reprehenderit esse mollit ullamco ullamco fugiat consequat nulla nostrud. Nisi dolore veniam proident ea eiusmod mollit excepteur. Laborum esse minim est proident ex velit ut eu culpa.
+          </p>
+        </EditorHook>
+      </div>
     )
   }
 }
+
+export default App
 ```
 
 ## License
--- example/src/App.js
+++ example/src/App.js
@@ -2,46 +2,31 @@ import React, { Component } from 'react'
 
 import { EditorHook } from 'react-revise'
 
-import Title from './Title'
-import Button, { SuperButton } from './Button'
-
-import { Provider } from 'react-redux'
-
-export default class App extends Component {
+class App extends Component {
   state = {
     editable: false
   }
 
-
-
   render () {
     return (
       <div>
-        <input type="checkbox" onChange={(e) => this.setState({
+        <input id="checkbox" type="checkbox" onChange={(e) => this.setState({
           editable: e.target.checked
         })} />
-        
+        <label for="checkbox"> edit</label>
+
         <EditorHook editable={this.state.editable}>
           <h1>Hello, World!</h1>
         </EditorHook>
 
         <EditorHook editable={this.state.editable}>
-          <span>
-            Dolor pariatur reprehenderit minim laborum enim irure qui adipisicing eiusmod.
-          </span>
+          <p>
+            Tempor mollit dolore culpa occaecat labore voluptate Lorem esse. Reprehenderit esse mollit ullamco ullamco fugiat consequat nulla nostrud. Nisi dolore veniam proident ea eiusmod mollit excepteur. Laborum esse minim est proident ex velit ut eu culpa.
+          </p>
         </EditorHook>
-
-        <EditorHook editable={this.state.editable}>
-          <div>
-            Dolor pariatur reprehenderit minim laborum enim irure qui adipisicing eiusmod.
-          </div>
-        </EditorHook>
-
-        <div>
-          Lorem do eu aute elit cupidatat exercitation et non minim. Laborum nulla dolor voluptate sit officia consectetur fugiat commodo ad cillum est laborum. Enim laborum reprehenderit enim duis ad sunt culpa laboris magna reprehenderit sit laboris et. Amet aliquip duis laborum laborum cillum tempor aliquip ut occaecat aliqua reprehenderit ut. Dolor veniam exercitation occaecat magna dolore aute ipsum ea in in. Non culpa laborum nulla do non consectetur enim do cupidatat nulla proident sint commodo cillum ea. Tempor labore ex excepteur sint ipsum ipsum consectetur cillum cillum occaecat incididunt deserunt excepteur deserunt aliqua pariatur.
-          <EditorHook editable={this.state.editable}><span>Final sentence. Boom.</span></EditorHook>
-        </div>
       </div>
     )
   }
 }
+
+export default App
